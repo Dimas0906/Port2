bdefd8293d997dccfef06ee142f228f015e5c717
Dmaul0906 <dimasmaulana0906@gmail.com>
2021-11-17 22:35:20 +0700

    initial commit

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./home.scss";
+
+function Home() {
+  return (
+    <div>
+      <h1>This is Home, don't forget it</h1>
+    </div>
+  );
+}
+
+export default Home;
