7fbed3e600b3f1b3bca6987246915a3d19b96a9e
Dmaul0906 <dimasmaulana0906@gmail.com>
2021-11-24 06:36:26 +0700

    latest update, before move to another proj. check point on experience

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,61 +1,28 @@
 import React from "react";
 import "./experience.scss";
+import { AiFillStar } from "react-icons/ai";
 
 function Experience() {
   return (
     <div className="experience-container">
-      <h1>Experience</h1>
-      <div className="timeline">
-        <div className="line"></div>
-        <div className="section">
-          <div className="bead"></div>
-          <div className="content">
-            <h2>Web Design</h2>
+      <h1 className="title">Experience</h1>
+      <div id="timeline">
+        <div className="timeline-item">
+          <div className="timeline-icon">
+            <AiFillStar />
+          </div>
+          <div className="timeline-content">
+            <h1>Lorem ipsum dolor</h1>
             <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim
-              totam neque molestias aperiam dolores ipsam libero necessitatibus,
-              reiciendis aut dolor facere explicabo aliquam praesentium sapiente
-              id! Impedit atque animi ipsa?
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed unde
+              quia laborum voluptates omnis enim perferendis natus molestias
+              amet repellendus?
             </p>
+            <a classname="btn" href="#0">
+              button
+            </a>
           </div>
         </div>
-        <div className="section">
-          <div className="bead"></div>
-          <div className="content">
-            <h2>Web Design</h2>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim
-              totam neque molestias aperiam dolores ipsam libero necessitatibus,
-              reiciendis aut dolor facere explicabo aliquam praesentium sapiente
-              id! Impedit atque animi ipsa?
-            </p>
-          </div>
-        </div>
-        <div className="section">
-          <div className="bead"></div>
-          <div className="content">
-            <h2>Web Design</h2>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim
-              totam neque molestias aperiam dolores ipsam libero necessitatibus,
-              reiciendis aut dolor facere explicabo aliquam praesentium sapiente
-              id! Impedit atque animi ipsa?
-            </p>
-          </div>
-        </div>
-        <div className="section">
-          <div className="bead"></div>
-          <div className="content">
-            <h2>Web Design</h2>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim
-              totam neque molestias aperiam dolores ipsam libero necessitatibus,
-              reiciendis aut dolor facere explicabo aliquam praesentium sapiente
-              id! Impedit atque animi ipsa?
-            </p>
-          </div>
-        </div>
-        bead
       </div>
     </div>
   );
